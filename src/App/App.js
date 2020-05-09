49db5e58bec1330c518abc8c5f3467ee1734e4b2
Sarah Holder <sr.holder@yahoo.com>
2020-05-09 14:35:26 -0500

    finished setup

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.scss';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>SCATT SURPRISE</h1>
+        <button className="btn btn-dark">Button</button>
+      </div>
+    )
+  }
+}
+
+export default App;
